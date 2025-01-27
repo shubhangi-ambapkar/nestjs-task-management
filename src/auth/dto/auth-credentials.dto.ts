0d25519b8ce5068997f8fbc215c7a51db4043ad6
Shubhangi Ambapkar <ambapkarshubhangi@gmail.com>
2025-01-27 22:14:46 +0530

    Adding Authentication and User creation

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialsDTO {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(15)
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(15)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is weak',
+  })
+  password: string;
+}
