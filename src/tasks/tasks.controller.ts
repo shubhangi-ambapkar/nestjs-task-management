09576ec88aaa37d70cd053b76eb3096c57e56a55
Shubhangi Ambapkar <ambapkarshubhangi@gmail.com>
2025-02-03 22:56:06 +0530

    Adding Logs for error and verbose

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -17,11 +17,13 @@ import { Task } from './task.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
+import { Logger } from '@nestjs/common';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
-  constructor(private taskService: TasksService) {}
+  private logger = new Logger('TaskController');
+  constructor(private readonly taskService: TasksService) {}
 
   @Get()
   getTask(
@@ -30,6 +32,9 @@ export class TasksController {
   ): Promise<Task[]> {
     // if we have filter defined, call taskService.getTaskWithFilters
     // Otherwise, just get all Tasks
+    this.logger.verbose(
+      `User "${user.username}" retriving all Tasks. Filters ${JSON.stringify(filterDto)}`,
+    );
     if (Object.keys(filterDto).length) {
       return this.taskService.getTaskWithFilters(filterDto, user);
     } else {
@@ -47,6 +52,9 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" creating Task. Filters ${JSON.stringify(createTaskDto)}`,
+    );
     return this.taskService.createTask(createTaskDto, user);
   }
 
